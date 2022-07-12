40dc5a73c1f8fee7f68362dbc225c3a779a9edc0
Avazbekdev97 <avazbekdev571@gmail.com>
2022-07-12 10:25:24 +0500

    node_modules be gone!

---
+++ src/index.server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const env = require('dotenv')
+const app = express()
+
+env.config()
+
+app.use(express.json())
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Hello from Server"
+    })
+})
+
+app.post('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    })
+})
+
+app.listen(process.env.PORT, () => console.log(`Server is running on http://localhost:${process.env.PORT}`))
